0d54dc4f4054ed61b39731b295ba70947751a85d
Phelipe Matheus Andrade <111017521+PhelipeMatheus13@users.noreply.github.com>
2026-08-10 19:10:34 -0300

    refactor(user.schemas): add OpenAI documentation

--- src/modules/user/user.schemas.ts
+++ src/modules/user/user.schemas.ts
@@ -1,15 +1,19 @@
-import * as z from "zod";
+import { z } from "zod";
+import registry  from "../../shared/docs/registry.js";
 
-const registerSchema = z
-    .object({
+const registerSchema = registry.register(
+    "RegisterInput",
+    z.object({
         name: z
             .string()
             .trim()
-            .min(3, "Name must be at least 3 characters long"),
+            .min(3, "Name must be at least 3 characters long")
+            .openapi({example: "John Doe"}),
 
         email: z
             .email("Please provide a valid email address")
-            .trim(),
+            .trim()
+            .openapi({example: "johndoe@hotmail.com"}),
 
         password: z
             .string()
@@ -17,20 +21,37 @@ const registerSchema = z
             .regex(
                 /[!@#$%^&*(),.?":{}|<>]/,
                 "Password must contain at least one special character",
-            ),
+            )
+            .openapi({example: "Str0ng!Pass"}),
 
-        confirmPassword: z.string(),
+        confirmPassword: z.string().openapi({ example: "Str0ng!Pass" }),
     })
     .refine(
         (data) => data.password === data.confirmPassword,
         { path: ["confirmPassword"], message: "Passwords do not match" }
-    );
+    ),
+);
+
+
+const userSchema = registry.register(
+    "User",
+    z.object({
+        id: z.string(),
+        name: z.string(),
+        email: z.string(),
+        createdAt: z.string(),
+        updatedAt: z.string(),
+    })
+);
    
 // Represents the validated request body received by the API.
 // This type belongs to the transport layer and may differ from
 // the domain input used by the service layer.
 type RegisterRequest = z.infer<typeof registerSchema>;    
 
-export { registerSchema };
+export { 
+    registerSchema, 
+    userSchema,
+};
 
 export type { RegisterRequest };
